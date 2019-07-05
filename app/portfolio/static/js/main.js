86e024cfe20af839b9bbec6e25adb87f75404b5a
Byidi <sajot.vincent@gmail.com>
2019-07-05 16:26:17 +0200

    fix js

--- app/portfolio/static/js/main.js
+++ app/portfolio/static/js/main.js
@@ -20,33 +20,7 @@ function windowMove(e){
     var windowDiv = document.querySelector('.window');
     var header = document.querySelector('header');
     var posTop = parseInt(e.clientY) - parseInt(header.clientHeight) - 25 - 2;
-    var posLeft = parseInt(e.clientX) - ( parseInt(e.clientX) - parseInt(windowDiv.offsetLeft) );;
+    var posLeft = parseInt(e.clientX) - ( parseInt(e.clientX) - parseInt(windowDiv.offsetLeft) );
     windowDiv.style.top = posTop + 'px';
     windowDiv.style.left = posLeft + 'px';
-    // windowDiv.style.left = (e.clientX )+ 'px';
-    // console.log(windowDiv.offsetTop);
-    // windowDiv.style.top = (e.clientY - windowDiv.offsetTop )+ 'px';
-    // windowDiv.style.left = (e.clientX - windowDiv.offsetLeft )+ 'px';
 }
-
-/* RESIZE WINDOW DIV */
-
-// if(document.querySelector('.window') != null){
-//     document.querySelector('#resizeWindow').addEventListener('click', function(){
-//         var windowDiv = document.querySelector('.window');
-//         windowDiv.style.top = '0px';
-//         windowDiv.style.left = '0px';
-//     });
-// }
-
-//
-document.querySelector('.window .bar').addEventListener('click', function(e){
-    var windowDiv = document.querySelector('.window');
-    var header = document.querySelector('header');
-    console.log("Window Left " + windowDiv.offsetLeft);
-    console.log("E Left " + e.clientX);
-    var pos = parseInt(e.clientX) - ( parseInt(e.clientX) - parseInt(windowDiv.offsetLeft) );
-    console.log("POS : "+ pos);
-    console.log('--------------------------------');
-
-});
